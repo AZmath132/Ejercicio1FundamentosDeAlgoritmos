5d6476b69d806ad78f571a59ab63b2bc50e237a9
swank <swank@MATH>
2024-11-15 12:08:04 -0500

    Add project files.

---
+++ Productos.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    internal class Productos
+    {
+        string[] nombre = new string[0];
+        double[] precio = new double[0];
+        int posicion = 0;
+        public int buscar(string nom)
+        {
+            int indice = Array.IndexOf(nombre, nom);
+            return indice;
+        }
+        public void insertar()
+        {
+            Console.Write("Ingrese nombre: ");
+            string nom = Console.ReadLine().Trim();
+            if(buscar(nom)==-1)
+            {
+                Array.Resize(ref nombre, nombre.Length + 1);
+                Array.Resize(ref precio, precio.Length + 1);
+                nombre[posicion] = nom;
+                Console.Write("Ingrese precio: ");
+                double pre= double.Parse(Console.ReadLine());
+                precio[posicion] = pre;
+                posicion++;
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nEl producto agregado correctamente");
+                Console.ResetColor();
+            }
+        }
+        public void mostrar()
+        {
+            Console.SetCursorPosition(0, 14);
+            Console.Write("POSICIÓN");
+            Console.SetCursorPosition(10, 14);
+            Console.Write("NOMBRE");
+            Console.SetCursorPosition(30, 14);
+            Console.Write("PRECIO");
+            for(int i=0; i<nombre.Length; i++)
+            {
+                Console.SetCursorPosition(0, 15+i);
+                Console.Write(i);
+                Console.SetCursorPosition(10, 15 + i);
+                Console.Write(nombre[i]);
+                Console.SetCursorPosition(30, 15 + i);
+                Console.Write(precio[i]);
+            }
+        }
+        public void eliminar()
+        {
+            Console.Write("\nIngrese nombre a eliminar: ");
+            string nom = Console.ReadLine().Trim();
+            if(buscar(nom)!= -1)
+            {
+                for(int i=buscar(nom); i<nombre.Length-1;i++)
+                {
+                    nombre[i] =nombre[i+1];
+                    precio[i] = precio[i + 1];
+                }
+                Array.Resize(ref nombre, nombre.Length - 1);
+                Array.Resize(ref precio, precio.Length - 1);
+                posicion--;
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nEl producto eliminado correctamente");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nEl producto no se puede eliminar porque no existe");
+                Console.ResetColor();
+            }
+        }
+        public void ordenar()
+        {
+            for(int i=0; i<nombre.Length-1; i++)
+            {
+                for(int j=0; j<nombre.Length -1 -i;j++)
+                {
+                    if(string.Compare(nombre[j],nombre[j+1])>0)
+                    {
+                        string temp=nombre[j];
+                        double temp2=precio[j];
+                        nombre[j] = nombre[j + 1];
+                        precio[j] = precio[j + 1];
+                        nombre[j + 1] = temp;
+                        precio[j + 1] = temp2;
+                    }
+                }
+            }
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nProductos ordenados correctamente");
+            Console.ResetColor();
+        }
+        public void modificar()
+        {
+            Console.Write("\nIngrese nombre a modificar: ");
+            string nom = Console.ReadLine().Trim();
+            if(buscar(nom)!= -1)
+            {
+                int indice = buscar(nom);
+                Console.Write("\nIngrese el nuevo nombre: ");
+                string nuevoNom=Console.ReadLine().Trim();
+                Console.Write("Ingrese nuevo precio: ");
+                double nuevoPore = double.Parse(Console.ReadLine());
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nEl producto no se puede modificar porque no existe");
+                Console.ResetColor();
+            }
+        }
+    }
+  
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte opcion;
+           string regresar;
+            Productos p = new Productos();
+            do
+            {
+
+                Console.WriteLine("BIENVENIDO AL SISTEMA DE REGISTRO DE PRODUCTOS\n");
+
+                Console.WriteLine("******** MENÚ DE OPCIONES *******");
+
+                Console.WriteLine("* 1 - Insertar         *");
+
+                Console.WriteLine("* 2 - Mostrar          *");
+
+                Console.WriteLine("* 3 - Eliminar         *");
+
+                Console.WriteLine("* 4 - Ordenar          *");
+
+                Console.WriteLine("* 5 - Modificar         *");
+
+                Console.WriteLine("* 6 - Buscar          *");
+
+                Console.WriteLine("* 0 - Salir           *");
+
+                Console.WriteLine("*********************************\n");
+
+
+
+                Console.Write("Ingrese una opción: ");
+
+
+
+                while (!byte.TryParse(Console.ReadLine(), out opcion) || opcion > 6)
+
+                {
+
+                    Console.Write("Error: Ingrese una opción: ");
+
+                }
+
+
+
+                switch (opcion)
+
+                {
+
+                    case 0: Environment.Exit(0); break;
+
+                    case 1: p.insertar();  break;
+
+                    case 2:p.mostrar(); break;
+
+                    case 3: p.eliminar();  break;
+
+                    case 4:p.ordenar(); break;
+
+                    case 5:p.modificar(); break;
+
+                    case 6: break;
+
+                }
+
+
+
+                Console.Write("\n\nPara regresar al menú [si] para salir cualquier otra tecla: ");
+
+                regresar = Console.ReadLine();
+
+                Console.Clear();
+
+            } while (regresar == "si");
+
+            Console.ReadKey();
+        }
+    }
+}
